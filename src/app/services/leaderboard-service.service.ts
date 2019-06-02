e149ec1760904e272441925a9ad8ee3dc1132e67
unknown <davidjmartin@hotmail.com>
2019-06-02 23:58:28 +0100

    updared services to call poe-backend heroku server

--- src/app/services/leaderboard-service.service.ts
+++ src/app/services/leaderboard-service.service.ts
@@ -11,33 +11,51 @@ import { LeaderboardModel } from '../models/LeaderboardModel';
 export class LeaderboardService{
 
   constructor(private http: HttpClient) { }
+import { TableColumnModel } from './../models/TableColumnModel';
+import { Observable } from 'rxjs';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { LeagueNameModel } from '../models/LeagueNameModel';
+import { LeaderboardModel } from '../models/LeaderboardModel';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LeaderboardService{
+
+  constructor(private http: HttpClient) { }
+
+  API_HEROKU_URL = 'https://poe-ladder-db.herokuapp.com/';
+  API_LOCALHOST_URL = 'http://localhost:8080/';
+  API = this.API_HEROKU_URL;
 
   public getDelveLeaderboards(leagueName: string): Observable<LeaderboardModel[]> {
-    const url = 'http://localhost:8080/leaderboards?leagueName='+leagueName+'&leaderboard=Top Delve Depths';
+    const url = this.API + 'leaderboards?leagueName='+leagueName+'&leaderboard=Top Delve Depths';
     console.log("service.getDelveLeaderboards() url call to : " + url);
     return this.http.get<LeaderboardModel[]>(url);
   }
 
   public getRaceTo100Leaderboards(leagueName: string): Observable<LeaderboardModel[]> {
-    const url = 'http://localhost:8080/leaderboards?leagueName='+leagueName+'&leaderboard=Top Race to 100';
+    const url = this.API + 'leaderboards?leagueName='+leagueName+'&leaderboard=Top Race to 100';
     console.log("service.getRaceTo100Leaderboards() url call to : " + url);
     return this.http.get<LeaderboardModel[]>(url);
   }
 
   public getUberlabLeaderboards(leagueName: string): Observable<LeaderboardModel[]> {
-    const url = 'http://localhost:8080/leaderboards?leagueName='+leagueName+'&leaderboard=Top UberLab Times';
+    const url = this.API + 'leaderboards?leagueName='+leagueName+'&leaderboard=Top UberLab Times';
     console.log("service.getUberlabLeaderboards() url call to : " + url);
     return this.http.get<LeaderboardModel[]>(url);
+
   }
 
   public getLeaderboardLadder(league: string, leaderboard: string): Observable<LeaderboardModel[]> {
-    const url = 'http://localhost:8080/leaderboard-ladder?leagueName='+league+'&leaderboard='+leaderboard;
+    const url = this.API + 'leaderboard-ladder?leagueName='+league+'&leaderboard='+leaderboard;
     console.log('service recieved request for : '+url);
     return this.http.get<LeaderboardModel[]>(url);
   }
 
   public getLeagueNames(): Observable<LeagueNameModel[]> {
-    const url = 'http://localhost:8080/league-names';
+    const url = this.API + 'league-names';
     console.log("getLeagueNames() called");
     return this.http.get<LeagueNameModel[]>(url);
   }
@@ -46,20 +64,23 @@ export class LeaderboardService{
     console.log("getLeaderboardTableColumns() league : " + league);
     let url = "";
     if(league.includes("Delve")) {
-      url = 'http://localhost:8080/leaderboard-columns-delve';
+      url = this.API + 'leaderboard-columns-delve';
     } else if(league.includes("UberLab")){
-      url = 'http://localhost:8080/leaderboard-columns-uberlab';
+      url = this.API + 'leaderboard-columns-uberlab';
     } else {
-      url = 'http://localhost:8080/leaderboard-columns-raceto100';
+      url = this.API + 'leaderboard-columns-raceto100';
     }
     console.log("getLeaderboardTableColumns() url : " + url);
     return this.http.get<TableColumnModel[]>(url);
   }
 
   public getCustomLeagueLeaderboard(customLeagueName: string): Observable<LeaderboardModel[]> {
-    const url = 'http://localhost:8080/custom-league?leagueName='+customLeagueName;
+    const url = this.API + 'ustom-league?leagueName='+customLeagueName;
     console.log("service.getCustomLeagueLeaderboard() url call to : " + url);
     return this.http.get<LeaderboardModel[]>(url);
   }
+
+}
+
 
 }
